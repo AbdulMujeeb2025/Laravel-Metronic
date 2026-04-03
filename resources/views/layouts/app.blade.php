e10e22d93cc1bd2e2fe7bb21b2e8114a9f919f3d
AbdulMujeeb2025 <abdulmujeebkhanniazi@gmail.com>
2026-04-03 17:46:19 +0500

    update auth scaffolding

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4836,6 +4836,17 @@
 									<!--end::Menu wrapper-->
 								</div>
 								<!--end::User menu-->
+								<!-- ========== LOGOUT BUTTON ========== -->
+<div class="app-navbar-item ms-1 ms-md-4">
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+        <button type="submit" class="btn btn-sm btn-danger fw-bold">
+            <i class="ki-duotone ki-exit-right-corner fs-2 me-1"></i>
+            Logout
+        </button>
+    </form>
+</div>
+<!-- ========== LOGOUT BUTTON END ========== -->
 								<!--begin::Header menu toggle-->
 								<div class="app-navbar-item d-lg-none ms-2 me-n2" title="Show header menu">
 									<div class="btn btn-flex btn-icon btn-active-color-primary w-30px h-30px" id="kt_app_header_menu_toggle">
